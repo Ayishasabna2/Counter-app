99273cdfd204fca42d05e0137f1d10eacd6bbbcb
Ayishasabna2 <ayishasabna808@gmail.com>
2024-02-01 20:55:15 +0530

    Counter app

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Box, Button, Container, Grid, Stack } from "@mui/material";
+import React, { useState } from 'react';
+
+export default function () {
+
+  let [counter,setCounter] = useState(0);
+
+
+  return (
+    <>
+   
+      <Box sx={{m:5}} >
+        <Container >
+          <Grid container justifyContent= "center" >           
+            <Stack direction={"column"} spacing={2}>
+              <h1>Counter App</h1>
+              <h2>{counter < 0 ? 0 : counter}</h2>
+              <Stack direction={'row'} spacing={2}>
+                <Button variant="contained" onClick={()=>setCounter(--counter)} disabled={counter === 0} >-</Button>
+                <Button variant="contained" onClick={()=>setCounter(++counter)} >+</Button>
+              </Stack>
+            </Stack>
+          </Grid>
+        </Container>
+      </Box>
+    
+    </>
+  )
+}
+
+
+   
+
+
